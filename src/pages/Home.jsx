e4841c617f7e712c4404b07edfc5fad629da93ff
simphiwek <konaolwethu@gmail.com>
2024-01-09 18:15:10 +0200

    Home page changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,27 @@
 import './styles/style.css';
 import { Link, animateScroll as scroll } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="about-us" smooth={true} duration={500}>About Us</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="products" smooth={true} duration={500}>Products</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div> 
-        </nav>
+        <div className="navigation">
+            <Link to="about-us"
+                smooth={true}
+                duration={300}
+                className="nav">About Us</Link>
+
+            <Link to="products"
+                smooth={true}
+                duration={300}
+                className="nav">Products</Link>
+        </div>
     )
 }
 const Header = () => {
-    return(
+    return (
         <section className="header">
             <div className="container">
                 <div className="row">
@@ -36,7 +36,7 @@ const Header = () => {
 }
 
 const AboutUs = () => {
-    return(
+    return (
         <section className="about-us" id='about-us'>
             <div className="container">
                 <h1>About Us</h1>
@@ -46,7 +46,7 @@ const AboutUs = () => {
 }
 
 const Products = () => {
-    return(
+    return (
         <section className="products" id='products'>
             <div className="container">
                 <h1>Products</h1>
@@ -54,14 +54,52 @@ const Products = () => {
         </section>
     )
 }
+const Footer = () => {
+    return(
+        <section className="footer" id='footer'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 pr-md-5">
+                        <Link to="" className="footer-logo d-block mb-4">Linked Digital Computers</Link>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quasi perferendis ratione perspiciatis accusantium.</p>
+                    </div>
+                    <div className="col-md">
+                        <ul className="list-unstyled nav-links">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">About Us</Link></li>
+                            <li><Link to="/">Product</Link></li>
+                            <li><Link to="/">Contact</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-md">
+                        <ul className="list-unstyled nav-links">
+                            <li><Link to="/">Privacy</Link></li>
+                            <li><Link to="/">Terms &amp; Conditions</Link></li>
+                            <li><Link to="/">Partners</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-md text-md-center">
+                        <ul className="social list-unstyled">
+                            <li><Link to=""><FontAwesomeIcon icon={faCoffee} style={{color:"#fff"}}/></Link></li>
+                            <li><Link to=""><FontAwesomeIcon icon={faCoffee} style={{color:"#fff"}}/></Link></li>
+                            <li><Link to=""><FontAwesomeIcon icon={faCoffee} style={{color:"#fff"}}/></Link></li>
+                            <li><Link to=""><FontAwesomeIcon icon={faCoffee} style={{color:"#fff"}}/></Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
 
 const Home = () => {
-    return(
+    return (
         <div>
             <Navbar />
             <Header />
             <AboutUs />
             <Products />
+            <Footer />
         </div>
     )
 }
