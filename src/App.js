47511a9577923a84f3359c403de51a29fc2bad5b
simphiwek <konaolwethu@gmail.com>
2024-01-09 12:35:29 +0200

    header rename and favicon image change and setting how page to route to Home

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import './App.css';
-import { Button, Navbar } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Quotes from './pages/Quotes.jsx';
 import Invoice from './pages/Invoice.jsx';
 import Product from './pages/Product.jsx';
 import AddItem from './pages/AddItems.jsx';
-import { useState } from 'react';
+import Home from './pages/Home.jsx';
+
 
 const App = () => {
 
-  const [sideVisible, setSideVisible] = useState(true);
-
-  const toggleSideNav = () => {
-    setSideVisible(!sideVisible);
-  };
+ 
   
   return (
     <div className="App">
       <Router>
         <div>
-          <button onClick={toggleSideNav}>Toggle Nav</button>
-          <div className={`side-nav ${sideVisible ? 'visible' : ''}`}>
-            <Link to="/">Home</Link>
-            <Link to="/quote">Quote</Link>
-            <Link to="/invoice">Invoice</Link>
-            <Link to="/products">Products</Link>
-          </div>
           <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="" element={<Home />} />
             <Route path="/quote" element={<Quotes />} />
             <Route path="/invoice" element={<Invoice />} />
             <Route path="/products" element={<Product />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './Component.css';
+
+function Navbar(){
+    return(
+        <div className='navigation'>
+            <Link to="/quote" className='nav'>Quotations</Link>
+            <Link to="/invoice" className='nav'>Invoices</Link>
+            <Link to="/products" className='nav'>Products</Link>
+        </div>
+    )
+}
+export default Navbar;
