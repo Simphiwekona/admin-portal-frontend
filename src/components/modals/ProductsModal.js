b4e229ecbc538ed44b7cf815687a67481b3ec720
simphiwek <simphiwe.kona@outlook.com>
2024-01-11 23:45:01 +0200

    product Model and product page

--- src/components/modals/ProductsModal.js
+++ src/components/modals/ProductsModal.js
@@ -2,11 +2,45 @@ import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
 const ProductsModal = () => {
-
     const [show, setShow] = useState(false);
+    const [productData, setProductData] = useState({
+        product: '',
+        model: '',
+        category: '',
+        status: ''
+    });
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:8080/api/products/adding", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(productData),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('success adding product');
+            handleClose();
+        })
+        .catch((error) => {
+            console.error('Error', error);
+            alert("Can't connect, Please try again!")
+        });
+    }
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setProductData((prevData) => ({
+            ...prevData,
+            [id]: value
+        }));
+    }
 
     return (
         <>
@@ -17,44 +51,44 @@ const ProductsModal = () => {
             <Modal show={show} onHide={handleClose} centered>
                 <Modal.Title style={{ textAlign: "center" }}>New Product</Modal.Title>
                 <Modal.Body>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3">
                             <Form.Label>Product Name</Form.Label>
-                            <Form.Control type="text" placeholder="Product Name" />
+                            <Form.Control type="text" id="product" placeholder="Product Name" onChange={handleChange} />
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Model</Form.Label>
-                            <Form.Control type="text" placeholder="Model" />
+                            <Form.Control type="text" id="model" placeholder="Model" onChange={handleChange} />
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Category</Form.Label>
-                            <Form.Select aria-label="Default select example">
+                            <Form.Select id="category" aria-label="Default select example" onChange={handleChange}>
                                 <option>Open this select menu</option>
-                                <option value="1">Laptop</option>
-                                <option value="2">Monitor</option>
-                                <option value="3">Desktop</option>
-                                <option value="4">other</option>
+                                <option value="Laptop">Laptop</option>
+                                <option value="Monitor">Monitor</option>
+                                <option value="Desktop">Desktop</option>
+                                <option value="Other">Other</option>
                             </Form.Select>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Status</Form.Label>
-                            <Form.Select aria-label="Default select example">
+                            <Form.Select id="status" aria-label="Default select example" onChange={handleChange}>
                                 <option>Open this select menu</option>
-                                <option value="1">Sold</option>
-                                <option value="2">Pending</option>
-                                <option value="3">Available</option>
+                                <option value="Sold">Sold</option>
+                                <option value="Pending">Pending</option>
+                                <option value="Available">Available</option>
                             </Form.Select>
                         </Form.Group>
+                        <Modal.Footer>
+                            <Button variant="success" type="submit">
+                                Add Product
+                            </Button>
+                        </Modal.Footer>
                     </Form>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="success">
-                        Add Product
-                    </Button>
-                </Modal.Footer>
             </Modal>
         </>
-    )
-
+    );
 }
+
 export default ProductsModal;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,56 +1,85 @@
-import React from 'react'
+import React from 'react';
 import ProductsModal from '../components/modals/ProductsModal';
 import Navbar from '../components/Navbar';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Product = () => {
+    const [productData, setProductData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchProductData = () => {
+        return fetch("http://localhost:8080/api/products/all")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network is down, try again later');
+                }
+                return response.json();
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+                throw error;
+            });
+    };
+
+    useEffect(() => {
+        const fetchDataAndHandleError = () => {
+            fetchProductData()
+                .then(data => setProductData(data))
+                .catch(error => console.error('Error setting products data: ', error))
+                .finally(() => setLoading(false));
+        };
+        fetchDataAndHandleError();
+
+        const intervaild = setInterval(fetchDataAndHandleError, 1000);
+        return () => clearInterval(intervaild);
+    }, []);
+
     return (
         <>
             <Navbar />
-            <nav class="navbar bg-body-tertiary mb-5">
+            <nav className="navbar bg-body-tertiary mb-5">
                 <div className="container-fluid">
                     <div>
                         <span className="navbar-brand mb-0 h1">Products</span>
                     </div>
-                    <div class="d-flex justify-content-end">
+                    <div className="d-flex justify-content-end">
                         <ProductsModal />
                     </div>
                 </div>
             </nav>
 
             <div className="container">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Product Name</th>
-                            <th scope="col">Modal</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
+                {loading ? (
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                ) : (
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Product Name</th>
+                                <th scope="col">Model</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {productData.map((product, index) => (
+                                <tr key={index}>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{product.product}</td>
+                                    <td>{product.model}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.status}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
         </>
-    )
-}
+    );
+};
+
 export default Product;
