35f3e6d677bdee4d961cbb25797357d55e53de67
simphiwek <simphiwe.olwethu@jumpco.io>
2024-04-06 19:39:51 +0200

    New features, Signup and navbar and home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMessage } from '@fortawesome/free-solid-svg-icons'
 import './Component.css';
 
 function Navbar(){
     return(
-        <div className='navigation'>
-            <Link to='/message' className='nav'><FontAwesomeIcon icon={faMessage} /></Link>
-            <Link to="/quote" className='nav'>Quotations</Link>
-            <Link to="/invoice" className='nav'>Invoices</Link>
-            <Link to="/products" className='nav'>Products</Link>
-        </div>
+        // <div className='navigation'>
+        //     <Link to="/quote" className='nav'>Quotations</Link>
+        //     <Link to="/invoice" className='nav'>Invoices</Link>
+        //     <Link to="/products" className='nav'>Products</Link>
+        // </div>
+        <></>
+
     )
 }
 export default Navbar;
